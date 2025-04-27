dbe9af76c1ef5f4343907d945257f1a72894062a
euandrelucaas <adg@adgdev.me>
2025-04-27 10:35:47 -0300

    feat: support a .env

--- src/anime/anime.service.ts
+++ src/anime/anime.service.ts
@@ -1,13 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
+import { ConfigService } from '@nestjs/config';
 import { Observable } from 'rxjs';
 import { AxiosResponse } from 'axios';
 
 @Injectable()
 export class AnimeService {
-  private baseUrl = 'http://154.53.33.246:5281';
+  private readonly baseUrl: string;
 
-  constructor(private readonly httpService: HttpService) {}
+  constructor(
+    private readonly httpService: HttpService,
+    private readonly configService: ConfigService,
+  ) {
+    this.baseUrl =
+      this.configService.get<string>('BASE_URL') || 'http://localhost:3000';
+  }
 
   searchAnime(query: string, page = 1): Observable<AxiosResponse<any>> {
     const url = `${this.baseUrl}/anime/zoro/${encodeURIComponent(query)}?page=${page}`;
--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AnimeModule } from './anime/anime.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [AnimeModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    AnimeModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
