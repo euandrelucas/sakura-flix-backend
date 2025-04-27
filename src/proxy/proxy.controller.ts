c2a3f179931cbdb80b80d57dc79c0884f4b6beac
euandrelucaas <adg@adgdev.me>
2025-04-27 12:36:22 -0300

    again: proxy

--- src/proxy/proxy.controller.ts
+++ src/proxy/proxy.controller.ts
@@ -1,22 +1,33 @@
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/no-unsafe-member-access */
-import { Controller, Get, Query, Res } from '@nestjs/common';
+import { Controller, Query, Res, Req, All } from '@nestjs/common';
 import { HttpService } from '@nestjs/axios';
-import { FastifyReply } from 'fastify';
+import { FastifyReply, FastifyRequest as Request } from 'fastify';
 import { firstValueFrom } from 'rxjs';
 
 @Controller('proxy')
 export class ProxyController {
   constructor(private readonly httpService: HttpService) {}
 
-  @Get()
+  @All()
   async proxy(
     @Query('url') url: string,
+    @Req() req: Request,
     @Res({ passthrough: false }) res: FastifyReply,
   ) {
+    // Se for uma preflight OPTIONS request
+    if (req.method === 'OPTIONS') {
+      res.header('Access-Control-Allow-Origin', '*');
+      res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+      res.statusCode = 204;
+      res.send();
+      return;
+    }
+
     if (!url) {
       res.statusCode = 400;
-      res.send('Missing url');
+      res.send('Missing url parameter');
       return;
     }
 
@@ -27,12 +38,18 @@ export class ProxyController {
         }),
       );
 
+      // Headers CORS
       res.header('Access-Control-Allow-Origin', '*');
-      res.header('Content-Type', response.headers['content-type']);
+      res.header('Access-Control-Allow-Methods', 'GET,OPTIONS');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
 
-      response.data.pipe(res.raw); // aqui você está assumindo o controle total
+      // Content-Type vindo da resposta original
+      res.header(
+        'Content-Type',
+        response.headers['content-type'] || 'application/octet-stream',
+      );
 
-      // NÃO retorna mais nada aqui!
+      response.data.pipe(res.raw);
     } catch (error) {
       console.error('Proxy error:', error.message);
       res.statusCode = 500;
